f279881cb93cee15a899cda986c00d2d5f3f3098
Tim B. <werpuzzlemoon@gmail.com>
2013-06-29 14:05:40 -0400

    Cleaned the code in game.lua.
    And, you know, made it work at all.

--- game.lua
+++ game.lua
@@ -1,29 +1,23 @@
+game = {}
+
 t = {{4,1,1000},{50,39,1700},{540,13,432},{12,13,1600}}  --x, y, time to stay alive
-debounce = true   --for detecting "q"
+pts = love.graphics.newImage("catpix.png")    --a particle
 
-function love.load()
-	pts = love.graphics.newImage("catpix.png")    --a particle
-end
-
-function love.draw()
-	for i=1,1000,1 do
-		if t[i] ~= nil then
-			love.graphics.draw(pts, t[i][1], t[i][2])
-			t[i][3] = t[i][3] - 1
-			if t[i][3] == 0 then t[i] = nil end
-		end
+function game.draw()
+	for i, foo in pairs(t) do
+		love.graphics.draw(pts, t[i][1], t[i][2])
 	end
 end
 
-function love.update()
-	for i=1,1000,1 do
-		if t[i] == nil then
-			maxT = i
-			break
-		end
+function game.keypressed(key)
+	if key == " " then
+		table.insert(t, {love.mouse.getX(), love.mouse.getY(), 1000})
 	end
-	if love.keyboard.isDown("q") and debounce == true then
-		t[maxT] = {love.mouse.getX(), love.mouse.getY(), 1000}
-		debounce = false
-	else debounce = true end
+end
+
+function game.update(dt)
+	for i, foo in pairs(t) do
+		t[i][3] = t[i][3] - 1
+		if t[i][3] == 0 then table.remove(t, i) end
+	end
 end
