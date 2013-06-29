85079aa3d722aa89d2e06aeef1d4ef664713a19b
finnbar <finnjkeating@aol.com>
2013-06-29 18:41:38 +0100

    Added particle system and cat picture.

--- game.lua
+++ game.lua
@@ -1,7 +1,29 @@
-game = {}
+t = {{4,1,1000},{50,39,1700},{540,13,432},{12,13,1600}}  --x, y, time to stay alive
+debounce = true   --for detecting "q"
 
-function game.update(dt)
+function love.load()
+	pts = love.graphics.newImage("catpix.png")    --a particle
 end
 
-function game.draw()
+function love.draw()
+	for i=1,1000,1 do
+		if t[i] ~= nil then
+			love.graphics.draw(pts, t[i][1], t[i][2])
+			t[i][3] = t[i][3] - 1
+			if t[i][3] == 0 then t[i] = nil end
+		end
+	end
+end
+
+function love.update()
+	for i=1,1000,1 do
+		if t[i] == nil then
+			maxT = i
+			break
+		end
+	end
+	if love.keyboard.isDown("q") and debounce == true then
+		t[maxT] = {love.mouse.getX(), love.mouse.getY(), 1000}
+		debounce = false
+	else debounce = true end
 end
