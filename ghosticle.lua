928a94a641d6bcc505918f7f7ce9345420d53dc0
Michał Sidor <michcioperz@autistici.org>
2013-12-08 00:37:54 +0100

    fun fun fun fun fun fun

--- ghosticle.lua
+++ ghosticle.lua
@@ -10,18 +10,39 @@ end
 
 function Ghosticle:draw()
 	love.graphics.setColor(self.color)
+	local bigmove = 90 * (self.death/self.initialdeath) + 10
+	local smallmove =75-0 (65 * (self.death/self.initialdeath) + 10)
 	self.anim:draw(partimage, self.x, self.y, 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x, self.y + 100 * (self.death/self.initialdeath), 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x, self.y - 100 * (self.death/self.initialdeath), 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x + 100 * (self.death/self.initialdeath), self.y, 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x - 100 * (self.death/self.initialdeath), self.y, 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x + 75 * (self.death/self.initialdeath), self.y + 75 * (self.death/self.initialdeath), 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x - 75 * (self.death/self.initialdeath), self.y + 75 * (self.death/self.initialdeath), 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x + 75 * (self.death/self.initialdeath), self.y - 75 * (self.death/self.initialdeath), 0, 0.6, 0.6, self.w/2, self.h/2)
-	self.anim:draw(partimage, self.x - 75 * (self.death/self.initialdeath), self.y - 75 * (self.death/self.initialdeath), 0, 0.6, 0.6, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x, self.y + bigmove, 0, 0.6, 0.6, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x, self.y - bigmove, 0, 0.6, 0.6, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x + bigmove, self.y, 0, 0.6, 0.6, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x - bigmove, self.y, 0, 0.6, 0.6, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x + smallmove, self.y + smallmove, 0, 0.3, 0.3, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x - smallmove, self.y + smallmove, 0, 0.3, 0.3, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x + smallmove, self.y - smallmove, 0, 0.3, 0.3, self.w/2, self.h/2)
+	self.anim:draw(partimage, self.x - smallmove, self.y - smallmove, 0, 0.3, 0.3, self.w/2, self.h/2)
 	love.graphics.setBlendMode("multiplicative")
-	love.graphics.setColor(self.color[1],self.color[2],self.color[3],200-200*self.death/self.initialdeath)
-	love.graphics.draw(circ,self.x,self.y, 0, 2.5* (self.death/self.initialdeath), 2.5* (self.death/self.initialdeath), 50, 50)
+	love.graphics.setColor(self.color[1],self.color[2],self.color[3],50)
+	love.graphics.draw(circ,self.x,self.y, 0, 2* (self.death/self.initialdeath), 2* (self.death/self.initialdeath), 50, 50)
+	if self.death < self.initialdeath/2 then
+		love.graphics.setColor(255-self.color[1],255-self.color[2],255-self.color[3],200*self.death/self.initialdeath)
+	else
+		love.graphics.setColor(255-self.color[1],255-self.color[2],255-self.color[3],200-200*self.death/self.initialdeath)
+	end
+	love.graphics.draw(circ,self.x,self.y + bigmove, 0, 1* (1-self.death/self.initialdeath), 1* (1-self.death/self.initialdeath), 50, 50)
+	love.graphics.draw(circ,self.x,self.y - bigmove, 0, 1* (1-self.death/self.initialdeath), 1* (1-self.death/self.initialdeath), 50, 50)
+	love.graphics.draw(circ,self.x + bigmove,self.y, 0, 1* (1-self.death/self.initialdeath), 1* (1-self.death/self.initialdeath), 50, 50)
+	love.graphics.draw(circ,self.x - bigmove,self.y, 0, 1* (1-self.death/self.initialdeath), 1* (1-self.death/self.initialdeath), 50, 50)
+	if self.death < self.initialdeath/2 then
+		love.graphics.setColor(self.color[1],self.color[2],self.color[3],200*self.death/self.initialdeath)
+	else
+		love.graphics.setColor(self.color[1],self.color[2],self.color[3],200-200*self.death/self.initialdeath)
+	end
+	love.graphics.draw(circ,self.x + smallmove,self.y + smallmove, 0, 1.5* (1-self.death/self.initialdeath), 1.5* (1-self.death/self.initialdeath), 50, 50)
+	love.graphics.draw(circ,self.x - smallmove,self.y + smallmove, 0, 1.5* (1-self.death/self.initialdeath), 1.5* (1-self.death/self.initialdeath), 50, 50)
+	love.graphics.draw(circ,self.x + smallmove,self.y - smallmove, 0, 1.5* (1-self.death/self.initialdeath), 1.5* (1-self.death/self.initialdeath), 50, 50)
+	love.graphics.draw(circ,self.x - smallmove,self.y - smallmove, 0, 1.5* (1-self.death/self.initialdeath), 1.5* (1-self.death/self.initialdeath), 50, 50)
+
 	love.graphics.setColor(255,255,255,255)
 	love.graphics.setBlendMode("alpha")
 end
