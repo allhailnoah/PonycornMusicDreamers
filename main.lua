2d7d2bc8045dee0074f12de83b0197d0b648ea37
Michał Sidor <michcioperz@autistici.org>
2013-12-10 17:17:51 +0100

    Made a message appear when focus is lost

--- main.lua
+++ main.lua
@@ -25,6 +25,10 @@ function love.draw()
 	love.graphics.setColor(0,0,0,pausedopac)
 	love.graphics.rectangle("fill", 0, 0, love.graphics:getWidth(), love.graphics:getHeight())
 	love.graphics.setColor(255,255,255,255)
+	if paused then
+		f:set(50)
+		love.graphics.printf("Game paused", 50, 285, 700, "center")
+	end
 	arc.clear_key()
 end
 
@@ -62,9 +66,9 @@ function love.keyreleased(key)
 	end
 end
 
-function love.focus(f)
+function love.focus(foc)
 	if gamestate~=splash then
-		if not f then
+		if not foc then
 			pausedopac = 170
 			love.audio.pause()
 			paused = true
