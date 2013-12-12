3ab5761463afbcbe5ae7c12bb890660661589c93
Michał Sidor <michcioperz@autistici.org>
2013-12-12 21:32:53 +0100

    Indent fix in conf.lua

--- conf.lua
+++ conf.lua
@@ -1,11 +1,11 @@
 function love.conf(t)
-    t.title = "Song of Sparks"
-    t.author = "Team Fimichi"
-    t.console = true
-    t.version = "0.8.0"
-    t.screen.width = 800
-    t.screen.height = 600
-    t.modules.joystick = false
-    t.modules.physics = false
+	t.title = "Song of Sparks"
+	t.author = "Team Fimichi"
+	t.console = true
+	t.version = "0.8.0"
+	t.screen.width = 800
+	t.screen.height = 600
+	t.modules.joystick = false
+	t.modules.physics = false
 	t.screen.fullscreen = false
 end
