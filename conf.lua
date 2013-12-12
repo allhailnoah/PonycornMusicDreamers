be72e6a4a46b4313a985589dd968e12e93c39bdd
Michał Sidor <michcioperz@autistici.org>
2013-12-12 21:35:19 +0100

    added some little things to make a better screenshots dir

--- conf.lua
+++ conf.lua
@@ -1,6 +1,8 @@
 function love.conf(t)
 	t.title = "Song of Sparks"
 	t.author = "Team Fimichi"
+	t.url = "http://allhailnoah.github.io/PonycornMusicDreamers"
+	t.identity = "fimichi_sparks"
 	t.console = true
 	t.version = "0.8.0"
 	t.screen.width = 800
